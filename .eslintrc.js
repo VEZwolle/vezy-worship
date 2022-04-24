59504d7f0bcc57be8b4bfd0cc72ab24dcdd61788
Jan-Paul Kleemans <jpkleemans@gmail.com>
2022-04-24 12:53:50 +0200

    Use vue3-recommended linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     '@vue/standard'
   ],
   parserOptions: {
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/require-default-prop': 'off'
   }
 }
