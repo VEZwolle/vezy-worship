0f083e0ba5ef3dfa983ff733112d362b4794c045
Jan-Paul Kleemans <jpkleemans@gmail.com>
2022-05-26 14:30:25 +0200

    Improve translation alignment

--- src/components/song/SongOutputBeamer.vue
+++ src/components/song/SongOutputBeamer.vue
@@ -2,10 +2,8 @@
   <svg class="song-output-beamer">
     <text y="6.6rem">
       <tspan v-for="(line, i) in lines" :key="i" x="50%" dy="4.4rem">{{ line }}</tspan>
-    </text>
-
-    <text v-if="settings.translation" y="24rem" class="translation">
-      <tspan v-for="(line, i) in translatedLines" :key="i" x="50%" dy="4.1rem">{{ line }}</tspan>
+      <tspan x="50%" dy="4rem">&nbsp;</tspan>
+      <tspan v-for="(line, i) in translatedLines" :key="i" x="50%" dy="4.4rem" class="translation">{{ line }}</tspan>
     </text>
   </svg>
 </template>
@@ -38,7 +36,7 @@ export default {
   width: 100%;
   height: 100%;
 
-  text {
+  tspan {
     font-size: 3.8rem;
     letter-spacing: 0.01rem;
 
@@ -54,7 +52,7 @@ export default {
     filter: shadow(0.8);
   }
 
-  text.translation {
+  tspan.translation {
     fill: #bbb;
     font-style: italic;
     stroke-width: 0.4rem;
