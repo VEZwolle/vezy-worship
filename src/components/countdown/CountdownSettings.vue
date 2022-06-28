5ec06faf0981eaaca1665b1752a4c4f997e73a52
J76F <github@jfaber.nl>
2022-06-28 20:10:50 +0200

    Opmerkingen verwerkt op boolean na ivm keuze array

--- src/components/countdown/CountdownSettings.vue
+++ src/components/countdown/CountdownSettings.vue
@@ -1,6 +1,6 @@
 <template>
   <q-card-section>
-    <q-input v-model="settings.time" :label="klokopties[settings.klok].omschrijving" outlined mask="time" :rules="['time']">
+    <q-input v-model="settings.time" :label="types[settings.types].definition" outlined mask="time" :rules="['time']">
       <template #append>
         <q-icon name="access_time" class="cursor-pointer">
           <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -10,8 +10,8 @@
       </template>
     </q-input>
     <q-option-group
-      v-model="settings.klok"
-      :options="klokopties"
+      v-model="settings.types"
+      :options="types"
       color="primary"
       inline
     />
@@ -25,16 +25,16 @@ export default {
   extends: BaseSettings,
   data () {
     return {
-      klokopties: [
+      types: [
         {
           label: 'Aftelklok',
           value: 0,
-          omschrijving: 'Aftellen tot (weergave bij tijd in de toekomst op dezelfde dag)'
+          definition: 'Aftellen tot (weergave bij tijd in de toekomst op dezelfde dag)'
         },
         {
           label: 'Klok',
           value: 1,
-          omschrijving: 'Klok tot (weergave bij tijd in de toekomst op dezelfde dag)'
+          definition: 'Klok tot (weergave bij tijd in de toekomst op dezelfde dag)'
         }
       ]
     }
@@ -43,8 +43,8 @@ export default {
     if (!this.settings.time) {
       this.settings.time = this.$store.service.time
     }
-    if (this.settings.klok == null) {
-      this.settings.klok = 0
+    if (this.settings.types === undefined) {
+      this.settings.types = 0
     }
   }
 }
