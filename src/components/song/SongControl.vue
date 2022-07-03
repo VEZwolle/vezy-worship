66a0eee620c23a74be16f6d0046782636d55f772
J76F <github@jfaber.nl>
2022-07-03 19:47:54 +0200

    ontbrekende koppen toegevoegd

--- src/components/song/SongControl.vue
+++ src/components/song/SongControl.vue
@@ -113,15 +113,26 @@ export default {
 }
 
 const labels = [
+  { key: 'intro', color: 'deep-purple' },
+  { key: 'introduction', color: 'deep-purple' },
   { key: 'verse', color: 'blue' },
   { key: 'vers', color: 'blue' },
   { key: 'couplet', color: 'blue' },
+  { key: 'pre-chorus', color: 'purple' },
   { key: 'chorus', color: 'purple' },
   { key: 'refrein', color: 'purple' },
   { key: 'bridge', color: 'green' },
   { key: 'tag', color: 'deep-purple' },
+  { key: 'tussenspel', color: 'deep-purple' },
+  { key: 'interlude', color: 'deep-purple' },
+  { key: 'misc', color: 'deep-purple' },
+  { key: 'ending', color: 'red-10' },
   { key: 'end', color: 'red-10' },
-  { key: 'eind', color: 'red-10' }
+  { key: 'eind', color: 'red-10' },
+  { key: 'slide', color: 'blue' },
+  { key: 'vamp', color: 'blue' },
+  { key: 'other', color: 'blue' },
+  { key: 'scripture', color: 'red-10' }
 ]
 
 function splitSong (text, linesPerSlide) {
