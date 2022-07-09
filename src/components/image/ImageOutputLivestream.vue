7b0c2a73ad055ab17d5787f84780f1d3dffa46ff
Jan-Paul Kleemans <jpkleemans@gmail.com>
2022-07-09 17:44:06 +0200

    Simplify ImageOutputLivestream

--- src/components/image/ImageOutputLivestream.vue
+++ src/components/image/ImageOutputLivestream.vue
@@ -1,36 +1,12 @@
-<template>
-  <div v-if="!clear" class="image" :style="style" />
-</template>
-
 <script>
-import BaseOutput from '../output/BaseOutput.vue'
+import ImageOutputBeamer from './ImageOutputBeamer.vue'
 
 export default {
-  extends: BaseOutput,
+  extends: ImageOutputBeamer,
   computed: {
     fileUrl () {
       return this.$store.media[this.settings.fileLivestreamId]
-    },
-    style () {
-      if (this.alpha) {
-        return {
-          backgroundColor: '#fff'
-        }
-      }
-
-      return {
-        backgroundImage: `url(${this.fileUrl})`
-      }
     }
   }
 }
 </script>
-
-<style scoped>
-.image {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-}
-</style>
