e49949874d6ce953ce705e9f1dbd2b9921edfd43
Jan-Paul Kleemans <jpkleemans@gmail.com>
2022-07-14 17:58:16 +0200

    Remove redundant NBV21

--- src/components/scripture/bibles.js
+++ src/components/scripture/bibles.js
@@ -1,5 +1,5 @@
 export default [
-  { id: 'nbv21', name: 'NBV21 (NBV21)' },
+  { id: 'nbv21', name: 'NBV21' },
   { id: 'nbv', name: 'Nieuwe Bijbelvertaling (NBV)' },
   { id: 'hsv', name: 'Herziene Statenvertaling (HSV)' },
   { id: 'nbg', name: 'NBG-vertaling 1951 (NBG)' },
