719adee5c8d52eac1ac595bfd523ef5d11faa260
J76F <github@jfaber.nl>
2022-07-23 20:13:30 +0200

    Basis - excl. inhoud

---
+++ src/components/help/HelpMain.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="text-h4 q-mb-md">
+      Basis VezyWorship
+    </div>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adi</p>
+  </div>
+</template>
--- src/components/layout/Toolbar.vue
+++ src/components/layout/Toolbar.vue
@@ -78,21 +78,30 @@
       class="q-ml-sm"
       @click="openAppSettings"
     />
+    <q-btn
+      flat
+      icon="help"
+      dense
+      class="q-ml-sm"
+      @click="openHelp"
+    />
   </q-toolbar>
 
   <ServiceSettingsDialog ref="serviceSettingsDialog" />
   <AppSettingsDialog ref="appSettingsDialog" />
+  <HelpDialog ref="helpDialog" />
 </template>
 
 <script>
 import ServiceSettingsDialog from '../service/ServiceSettingsDialog'
 import AppSettingsDialog from './AppSettingsDialog'
+import HelpDialog from '../help/HelpDialog.vue'
 import icon from 'assets/icon.svg'
 import PACKAGE from '../../../package.json'
 import MessageControl from '../message/MessageControl'
 
 export default {
-  components: { ServiceSettingsDialog, AppSettingsDialog, MessageControl },
+  components: { ServiceSettingsDialog, AppSettingsDialog, HelpDialog, MessageControl },
   setup () {
     return { icon, version: PACKAGE.version }
   },
@@ -125,6 +134,9 @@ export default {
     },
     openAppSettings () {
       this.$refs.appSettingsDialog.show()
+    },
+    openHelp () {
+      this.$refs.helpDialog.show()
     }
   }
 }
