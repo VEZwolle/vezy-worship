e5776f1246350c2d025f956a39a8b14e86a24be6
J76F <github@jfaber.nl>
2023-07-02 16:37:05 +0200

    update dependencies & dev local emulator firestore from production

---
+++ dev/emulator-get-data.ps1
@@ -0,0 +1,17 @@
+$rootFolder = '.\dev'
+$exportFolder = 'export-firestore'
+$googleBucket = 'export-import-vezy-worship'
+Write-Host "Test to see if local export folder [$rootFolder\$exportFolder] exists"
+if (Test-Path -Path $rootFolder\$exportFolder) {
+  Write-Host "local firestore export [$rootFolder\$exportFolder] Exists -> Remove old data"
+  Remove-Item $rootFolder\$exportFolder -Recurse -Force
+} else {
+  Write-Host "local firestore export [$rootFolder\$exportFolder] Don't Exists"
+}
+Write-Host "Get data from Google-cloud; Login:"
+gcloud auth login
+gcloud config set project vezy-worship
+Write-Host "Get data from Google-cloud bucket [gs://$googleBucket/$exportFolder]:"
+gsutil -m cp -r gs://$googleBucket/$exportFolder $rootFolder
+Write-Host "logout:"
+gcloud auth revoke
