824a54236d6a0eb999a746cb60cdd8eafbf9d9dd
J76F <github@jfaber.nl>
2023-08-11 22:29:23 +0200

    opmaak start compare

--- src/components/song/SongDatabaseCompareDialog.vue
+++ src/components/song/SongDatabaseCompareDialog.vue
@@ -7,23 +7,57 @@
         </q-toolbar-title>
         <q-btn v-close-popup flat round dense icon="close" />
       </q-toolbar>
-      <div class="row q-px-md q-pt-md">
+      <div class="row q-pa-md q-gutter-md">
         <div class="col">
-          <q-editor ref="newSong" v-model="newSong" />
-          <h5>Lied met wijzigingen</h5>
-          <div class="newsong" v-html="newSongComp" />
+          <h6>Lied met wijzigingen</h6>
+          <q-editor ref="newSong" v-model="newText" />
         </div>
         <div class="col">
-          <q-editor ref="dbSong" v-model="dbSong" />
-          <h5>Origineel (db) versie</h5>
-          <div class="dbsong" v-html="dbSongComp" />
+          <h6>Origineel (db) versie</h6>
+          <q-editor ref="dbSong" v-model="databaseText" />
         </div>
       </div>
+
+      <q-separator />
+
+      <div class="row q-pa-md q-gutter-md">
+        <q-card bordered class="col">
+          <q-card-section>
+            <div class="text-h6" v-text="'Titel new'" />
+            <div class="text-subtitle2" v-text="'Collecie en nr.'" />
+          </q-card-section>
+          <q-separator inset />
+          <q-card-section>
+            <div ref="compareTitleNew" class="col lyrics" v-html="newText" />
+          </q-card-section>
+        </q-card>
+        <q-card bordered class="col">
+          <q-card-section>
+            <div class="text-h6" v-text="'Titel verschil'" />
+            <div class="text-subtitle2" v-text="'Collecie en nr.'" />
+          </q-card-section>
+          <q-separator inset />
+          <q-card-section>
+            <div ref="compareTitleDiff" class="col lyrics diff" v-html="compareText" />
+          </q-card-section>
+        </q-card>
+        <q-card bordered class="col">
+          <q-card-section>
+            <div class="text-h6" v-text="'Titel database'" />
+            <div class="text-subtitle2" v-text="'Collecie en nr.'" />
+          </q-card-section>
+          <q-separator inset />
+          <q-card-section>
+            <div ref="compareTitleDatabase" class="col lyrics" v-html="databaseText" />
+          </q-card-section>
+        </q-card>
+      </div>
+
       <q-separator />
 
       <q-card-actions align="right">
-        <q-btn color="secondary" label="vergelijk" @click.stop="htmlDiff">
-          <q-tooltip>HtmlDiff</q-tooltip>
+        <q-btn color="secondary" label="Opslaan in database" @click.stop="save">
+          <q-tooltip>Geselecteerde liederen toevoegen, vervangen & opslaan in database</q-tooltip>
         </q-btn>
         <q-btn color="secondary" label="Annuleren" @click.stop="hide">
           <q-tooltip>Wijzigingen niet toepassen</q-tooltip>
@@ -43,13 +77,15 @@ export default {
   ],
   data () {
     return {
-      newSong: 'De <b>schepping</b> van hemel en aarde 1 1 In het begin schiep God de hemel en de aarde. 2 De aarde was nog woest en doods, en duisternis lag over de oervloed, maar Gods geest zweefde over het water.',
-      dbSong: 'De schepping van hemel en <i>aarde</i> 1 In het begin schiep God de hemel en de aarde.  2   De aarde was woest en doods, duisternis lag over de oervloed, en over het water zweefde Gods geest.',
-      newSongComp: '',
-      dbSongComp: ''
+      newText: 'De <b>schepping</b> van hemel en aarde 1 1 In het begin schiep God de hemel en de aarde. 2 De aarde was nog woest en doods, en duisternis lag over de oervloed, maar Gods geest zweefde over het water.',
+      databaseText: 'De schepping van hemel en <i>aarde</i> 1 In het begin schiep God de hemel en de aarde.  2   De aarde was woest en doods, duisternis lag over de oervloed, en over het water zweefde Gods geest.',
+      compareTab: 'tabCompareTitle'
     }
   },
   computed: {
+    compareText () {
+      return HtmlDiff(this.databaseText, this.newText)
+    }
   },
   methods: {
     async show () {
@@ -58,9 +94,8 @@ export default {
     hide () {
       this.$refs.dialogDatabase.hide()
     },
-    htmlDiff () {
-      this.newSongComp = HtmlDiff(this.dbSong, this.newSong)
-      this.dbSongComp = this.dbSong //  HtmlDiff(this.newSong, this.dbSong) // or use this.dbSong whitout css clean
+    save () {
+      // save database, nog uitwerken
     }
   }
 }
@@ -71,11 +106,16 @@ export default {
 .q-card {
   min-width: max(60vw, min(1152px, 95vw));
   min-height: 80vh;
+
+  .q-card {
+    min-width: 0;
+    min-height: 15vh;
+  }
 }
 </style>
 
 <style lang="scss">
-  .newsong {
+  .diff {
     ins {
       background: lightgreen;
       text-decoration: none;
@@ -85,16 +125,22 @@ export default {
     }
   }
 
-  .dbsong {
+  .diffhide {
     del {
       visibility: hidden;
     }
-    ins {
+    ins, mark {
       text-decoration: none;
     }
     ins ~ del {
       display: none;
     }
+  }
+
+  .lyrics {
+    height: 10vh;
+    overflow-y: auto;
+    overflow-x: hidden;
   }
 
 </style>
