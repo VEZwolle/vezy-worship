899ffb655c4899f8db948c0967132b947ece76a2
J76F <github@jfaber.nl>
2024-10-13 21:59:37 +0200

    imageIds only check voor bekende video ext est doorlaten

--- src/stores/service.js
+++ src/stores/service.js
@@ -256,14 +256,14 @@ export default defineStore('service', {
       return this.media[id]
     },
     getImageIds () {
-      const imageExt = ['png', 'jpg', 'gif', 'tga', 'tif']
+      // const imageExt = ['png', 'jpg', 'gif', 'tga', 'tif', 'jpeg']
       const videoExt = ['mp4', 'mov', 'avi', 'wmv', 'vob', 'mpg', 'mp2', 'mpv2', 'mpe', 'mpeg', 'mpev2']
       let mediaIds = Object.keys(this.media)
       if (mediaIds.length) {
         mediaIds = mediaIds.filter((id) => {
           const ext = id.split('.').pop().toLowerCase()
-          if (imageExt.includes(ext)) return true
           if (videoExt.includes(ext)) return false
+          // if (imageExt.includes(ext)) return true
           // Nu wannneer extentie niet bekend, vanuitgaan dat image is
           return true
         })
