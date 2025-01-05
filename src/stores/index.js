6dd06d202ab630159cf5887213aaefd67bdb9641
J76F <github@jfaber.nl>
2025-01-05 12:34:57 +0100

    defineStore

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,8 +1,22 @@
+import { defineStore } from '#q-app/wrappers'
 import { createPinia } from 'pinia'
 import { PiniaSharedState } from 'pinia-shared-state'
 
-const pinia = createPinia()
+/*
+ * If not building with SSR mode, you can
+ * directly export the Store instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Store instance.
+ */
 
-pinia.use(PiniaSharedState({}))
+export default defineStore((/* { ssrContext } */) => {
+  const pinia = createPinia()
 
-export default pinia
+  // You can add Pinia plugins here
+  // pinia.use(SomePiniaPlugin)
+  pinia.use(PiniaSharedState({}))
+
+  return pinia
+})
