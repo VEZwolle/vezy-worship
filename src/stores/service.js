f0e242d14ab2c4cfd10983d03fb1c821133ed832
J76F <github@jfaber.nl>
2025-01-11 20:46:27 +0100

    fix first time dubbelklik element

--- src/stores/service.js
+++ src/stores/service.js
@@ -177,9 +177,9 @@ export const useServiceStore = defineStore('service', {
 
       this.livePresentation = cloneDeep(presentation)
       // reactivity op output schermen gebeurd bij dubbelklik pas nadat iets is aangeklikt of gewijzigd
-      // voor nu toestgebruik tijdelijk naar preview en volgende cycle weer in live om reactivity te triggeren op andere schermen.
+      // voor nu toestgebruik tijdelijk naar preview en na 1 millisec terug  reactivity te triggeren op andere schermen.
       this.arrowKeyLocation = true // active arrow keys tijdelijk naar preview om live daarna te activeren.
-      setTimeout(() => { this.arrowKeyLocation = false }, 0) // active arrow keys naar live // run in the next event cycle.
+      setTimeout(() => { this.arrowKeyLocation = false }, 1) // active arrow keys naar live // run after 1milisec
       this.isOnlyLivestreamClear = false
       this.goLiveKey = presentation.id
 
