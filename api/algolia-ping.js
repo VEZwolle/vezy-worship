7afdd205fc48c98e86de0d66fd1042c03c41afc7
J76F <github@jfaber.nl>
2025-02-01 00:00:33 +0100

    remove firebase admin & fix result check

--- api/algolia-ping.js
+++ api/algolia-ping.js
@@ -2,19 +2,23 @@
 import { onSchedule } from 'firebase-functions/v2/scheduler'
 import { logger } from 'firebase-functions'
 
-// import admin from 'firebase-admin'
 import axios from 'axios'
 
-// Initialization
-// admin.initializeApp()
+// Test: onSchedule functions in firebase emulator locally
+// NOTE: Please ensure you are running the firebase emulator locally, or the shell may call the functions in Production !!
+// - Start firebase emulator
+// - firebase functions:shell
+// - firebase > setInterval(() => yourScheduledFunc(), 60000)
+// - Logout: cntr+C cntr+C
 
-// '00 01 10,25 * *' elke 10e en 25e van de maand om 01:00
+// '00 01 10,25 * *' elke 10e en 25e van de maand om 01:00 | minuut uur dag maand jaar
 export const algoliaPing = onSchedule({
   schedule: '00 01 10,25 * *',
   region: 'europe-west1'
 }, async () => {
   const search = 'Jesus'
 
+  console.log('algoliaPing start')
   try {
     const headers = {
       Authorization: process.env.VEZY_API_TOKEN
@@ -24,10 +28,9 @@ export const algoliaPing = onSchedule({
       search
     }, { headers })
 
-    if (result.hits) {
+    if (result.data.hits) {
       return logger.log('Song search Algolia finished, resultaten gevonden')
     }
-    
     return logger.log('Song search Algolia finished, geen resultaten gevonden')
   } catch {
     return logger.log('Error Song search Algolia')
