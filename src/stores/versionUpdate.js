0fe8e2535cff7a77a10e3d6b95eecc746824fd81
J76F <github@jfaber.nl>
2025-02-25 23:47:08 +0100

    fix add osc

--- src/stores/versionUpdate.js
+++ src/stores/versionUpdate.js
@@ -111,19 +111,20 @@ export function versionUpdate (service) {
       // eslint-disable-next-line
       case version < 2.0102 :
       service.presentations.forEach(presentation => {
-        if (!presentation.settings.osc) presentation.settings.osc = { clip: null }
+        if (!presentation.settings.osc) presentation.settings.osc = {}
+        if (!presentation.settings.osc.clip) presentation.settings.osc.clip = null
         switch (presentation.type) {
           case 'song':
-            presentation.settings.osc.text = presentation.settings.osc.text || null
-            presentation.settings.osc.translation = presentation.settings.osc.translation || null
+            if (!presentation.settings.osc.text) presentation.settings.osc.text = null
+              if (!presentation.settings.osc.translation) presentation.settings.osc.translation = null
             break
           case 'caption':
           case 'scripture':
-            presentation.settings.osc.text = presentation.settings.osc.text || null
-            presentation.settings.osc.title = presentation.settings.osc.title || null
+            if (!presentation.settings.osc.text) presentation.settings.osc.text = null
+              if (!presentation.settings.osc.title) presentation.settings.osc.title = null
             break
           case 'countdown':
-            presentation.settings.osc.text = presentation.settings.osc.text || null
+            if (!presentation.settings.osc.text) presentation.settings.osc.text = null
             break
           case 'image':
           case 'video':
